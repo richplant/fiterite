bcae8da142eafdadd955e06550793be4beb5d5e3
Richard Plant <repairist@gmail.com>
2020-02-12 16:58:52 +0000

    Prepare for deployment

--- fiterite/urls.py
+++ fiterite/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
-from fiterite import settings
+from django.conf import settings
 
 urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fiterite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fiterite.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
