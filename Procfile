b5ed5cd392387641b5b39975ebffcb526a3a4622
Richard Plant <repairist@gmail.com>
2020-02-12 17:09:28 +0000

    Update heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn fiterite.wsgi --log-file -
--- fiterite/settings/base.py
+++ fiterite/settings/base.py
@@ -15,8 +15,8 @@ import os
 DEBUG = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
